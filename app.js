3c71ca066c443ac4e01f5554655f0326c9c6e26b
BadrKali <badrkali1997@gmail.com>
2025-04-03 09:32:46 +0000

    refactor: apply middlewares & streamline routing for better structure

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import subscriptionRouter from './routes/subscription.route.js';
 import connectToDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 
 app.listen(PORT, async () => {
