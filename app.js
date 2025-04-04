60d5282d1709cc11aa138284a33045bbdc1ba0d0
BadrKali <badrkali1997@gmail.com>
2025-04-04 10:29:57 +0000

    apply rate limiting middle ware on all request

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import authRouter from './routes/auth.route.js';
 import subscriptionRouter from './routes/subscription.route.js';
 import connectToDB from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import rateLimiter from './middlewares/ratelimit.middleware.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(rateLimiter);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
