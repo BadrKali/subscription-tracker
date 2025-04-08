7087a074a4a08fbda8005eb82cce8fac306a2017
BadrKali <badrkali1997@gmail.com>
2025-04-08 09:32:09 +0100

    refactor: update reminder worker to use new email sending function

--- workers/reminderWorker.js
+++ workers/reminderWorker.js
@@ -3,35 +3,28 @@ import { Worker } from "bullmq"
 import redisConnection from "../config/redisClient.js"
 import User from "../models/user.model.js"
 import {EMAIL_SENDER, EMAIL_SENDER_PASSWORD} from "../config/env.js"
+import { sendRemiderEmail } from "../utils/send-email.js"
+import Subscription from "../models/subscription.model.js"
 
 
 const reminderWorkder = new Worker (
     "reminderQueue",
     async (job) => {
         const { userId, subscriptionId, daysBefore } = job.data;
+        const subscription = await Subscription.findById(subscriptionId)
         const user = await User.findById(userId)
         if(!user) {
             console.error(`User ${userId} not found`)
             return;
         }
         console.log(`Sending email reminder to ${user.email} (${daysBefore} days before renewal)`);
-        await sendEmail(user.email, `Your subscription renews in ${daysBefore} days!`);
+        await sendRemiderEmail(
+            user.email,
+            user.name,
+            subscription
+        )
     },
     { connection: redisConnection }
 );
-
-async function sendEmail(to, message) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: { user: EMAIL_SENDER, pass: EMAIL_SENDER_PASSWORD },
-    });
-    await transporter.sendMail({
-        from: EMAIL_SENDER,
-        to,
-        subject: "Subscription Reminder",
-        text: message,
-    });
-    console.log(`Email sent to ${to}`);
-}
 
 
