652b51f63874ffddd729b8a89309bb56ca9f54f6
BadrKali <badrkali1997@gmail.com>
2025-04-08 09:34:01 +0100

    feat : add new routes/controllers for getting all subs and delete subs

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -67,3 +67,66 @@ export const cancelSubscription = async (req, res, next) => {
         next(error)
     }
 }
+
+export const getAllSubscriptions = async (req, res, next) => {
+    try {
+        if(req.user.role !== 'admin') {
+            const error = new Error('Unauthorized');
+            error.statusCode = 401;
+            throw error;
+        }
+        const subscriptions = await Subscription.find();
+        res.status(200).json({
+            success: true,
+            data: subscriptions
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getSubscriptionDetails = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+        if(!subscription) {
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(subscription.user.toString() !== req.user.id) {
+            const error = new Error('Unauthorized');
+            error.statusCode = 401;
+            throw error;
+        }
+        res.status(200).json({
+            success: true,
+            data: subscription
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+        if(!subscription) {
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(subscription.user.toString() !== req.user.id) {
+            const error = new Error('Unauthorized');
+            error.statusCode = 401;
+            throw error;
+        }
+        await subscription.remove();
+        res.status(200).json({
+            success: true,
+            message: 'Subscription deleted successfully'
+        });
+    } catch (error) {
+        next(error)
+    }
+}
+
--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -1,27 +1,16 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { createSubscription, getUserSubscriptions, cancelSubscription } from '../controllers/subscription.controller.js';
+import { createSubscription, getUserSubscriptions, cancelSubscription, getAllSubscriptions, getSubscriptionDetails, deleteSubscription } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
+subscriptionRouter.get('/', authorize, getAllSubscriptions);
 
-subscriptionRouter.get('/', (req, res) => {
-    res.send({ message: 'GET all subscriptions' });
-});
-
-subscriptionRouter.get('/:id', authorize,(req, res) => {
-    res.send({ message: 'GET subscription details' });
-});
+subscriptionRouter.get('/:id', authorize, getSubscriptionDetails);
 
 subscriptionRouter.post('/', authorize, createSubscription);
 
-subscriptionRouter.put('/:id', (req, res) => {
-    res.send({ message: 'Update the subscription by ID' });
-});
-
-subscriptionRouter.delete('/:id', (req, res) => {
-    res.send({ message: 'Delete subscription' });
-});
+subscriptionRouter.delete('/:id', authorize, deleteSubscription);
 
 subscriptionRouter.get('/user/:id',authorize, getUserSubscriptions);
 
